32602f71b4717ea6082f322ef17e6737ee432565
S <shakibmohammad18@gmail.com>
2024-09-17 23:28:12 +0600

    startup

--- lib/views/pages/onboarding/widgets/onboarding2.dart
+++ lib/views/pages/onboarding/widgets/onboarding2.dart
@@ -11,7 +11,7 @@ class Onboarding2 extends StatelessWidget {
       children: [
         Image.asset("assets/onboarding/destination.png"),
         const SizedBox(
-          height: 25,
+          height: 25
         ),
         const MyCustomText(
           text: "Choose a Destination",
@@ -19,7 +19,7 @@ class Onboarding2 extends StatelessWidget {
           fWeight: FontWeight.bold,
         ),
         const SizedBox(
-          height: 20,
+          height: 20
         ),
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 30),
