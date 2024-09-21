a8cbb4c8286b670cc9602fbf34f486d5e7e3ef97
S <shakibmohammad18@gmail.com>
2024-09-21 18:42:08 +0600

    getx

--- lib/controller/getx/uiController/auth/otp.dart
+++ lib/controller/getx/uiController/auth/otp.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class OtpController extends GetxController {
-
   final TextEditingController otp1 = TextEditingController();
   final TextEditingController otp2 = TextEditingController();
   final TextEditingController otp3 = TextEditingController();
