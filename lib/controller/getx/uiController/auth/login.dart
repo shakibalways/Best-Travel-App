e4e616de89439492c4c8d8ea5a1c6322fa38d8c9
S <shakibmohammad18@gmail.com>
2024-09-22 23:43:17 +0600

    login

--- lib/controller/getx/uiController/auth/login.dart
+++ lib/controller/getx/uiController/auth/login.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
+
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
