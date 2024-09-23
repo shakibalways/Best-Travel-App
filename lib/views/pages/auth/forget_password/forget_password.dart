b6f53187852f220f6c5aaefaa99d73f0c0ae04d5
S <shakibmohammad18@gmail.com>
2024-09-23 22:41:06 +0600

    forget password

---
+++ lib/views/pages/auth/forget_password/forget_password.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ForgetPasswordPage extends StatelessWidget {
+  const ForgetPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
