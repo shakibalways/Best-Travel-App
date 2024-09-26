a9b166fde0f42652d319e4b523467b3bc419d173
S <shakibmohammad18@gmail.com>
2024-09-26 22:01:29 +0600

    clear data

--- lib/views/pages/auth/sign_up/sign_up.dart
+++ lib/views/pages/auth/sign_up/sign_up.dart
@@ -150,7 +150,7 @@ class SignUpPage extends StatelessWidget {
                       ),
                     ),
                   ],
-                )
+                ),
               ],
             ),
           ),
