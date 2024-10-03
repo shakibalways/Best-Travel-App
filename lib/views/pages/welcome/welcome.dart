6a46d0a16e55e41c270472f155c7bccbf49b70e1
S <shakibmohammad18@gmail.com>
2024-10-03 14:08:10 +0600

    welcome page error solve

--- lib/views/pages/welcome/welcome.dart
+++ lib/views/pages/welcome/welcome.dart
@@ -9,64 +9,68 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(
-        alignment: Alignment.bottomRight,
-        children: [
-          SizedBox(
-            height: Get.height,
-            width: Get.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset("assets/images/vector/top.png"),
-                const Text(
-                  "Welcome",
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 3,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 50),
-                  child: Image.asset("assets/images/vector/bottom.png"),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 30,
-            right: 50,
-            child: GestureDetector(
-              onTap: () {
-                Get.to(() => const OnboardingPage());
-              },
-              child: Container(
-                height: Get.height * 0.23,
-                width: Get.width * 0.23,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(1, 2),
-                      color: Colors.grey.shade300,
-                      blurRadius: 2,
-                      spreadRadius: 2,
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            SizedBox(
+              height: Get.height,
+              width: Get.width,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset("assets/images/vector/top.png"),
+                    const Text(
+                      "Welcome",
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 3,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50,top: 30),
+                      child: Image.asset("assets/images/vector/bottom.png"),
                     ),
                   ],
                 ),
-                child: const Center(
-                  child: Icon(
-                    Icons.arrow_right_alt_rounded,
-                    color: Color(0xff1BBA85),
-                    size: 40,
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              right: 50,
+              child: GestureDetector(
+                onTap: () {
+                  Get.to(() => const OnboardingPage());
+                },
+                child: Container(
+                  height: Get.height * 0.23,
+                  width: Get.width * 0.23,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(1, 2),
+                        color: Colors.grey.shade300,
+                        blurRadius: 2,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: const Center(
+                    child: Icon(
+                      Icons.arrow_right_alt_rounded,
+                      color: Color(0xff1BBA85),
+                      size: 40,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
